406ff3559c5c3f9386fcacb4a9f0c566585f262f
mufatech <fabosedesidiqat@gmail.com>
2024-06-19 22:39:17 +0100

    updated

--- run.py
+++ run.py
@@ -1,18 +1,18 @@
 from app import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run()
 
 
 
-from flask_migrate import Migrate, MigrateCommand
-from flask_script import Manager
-from app import create_app, db
+# from flask_migrate import Migrate, MigrateCommand
+# from flask_script import Manager
+# from app import create_app, db
 
-app = create_app()
-migrate = Migrate(app, db)
-manager = Manager(app)
-manager.add_command('db', MigrateCommand)
+# app = create_app()
+# migrate = Migrate(app, db)
+# manager = Manager(app)
+# manager.add_command('db', MigrateCommand)
 
-if __name__ == '__main__':
-    manager.run()
+# if __name__ == '__main__':
+#     manager.run()
