b923c9c8d8e479c70ad874c1b80925f7bb756124
mufatech <fabosedesidiqat@gmail.com>
2024-07-11 16:17:36 +0100

    Updated

--- table.py
+++ table.py
@@ -1,29 +1,36 @@
-import mysql.connector
+import psycopg2
 
 # Establish connection
-connection = mysql.connector.connect(
+connection = psycopg2.connect(
     host= 'dpg-cq7pkqdds78s73dbd020-a.oregon-postgres.render.com',
     user= 'nasfat_render_user',
-    passwd = 's4UqmMqfhJVnNBlLn8s0KdUowOrYklkE',
+    passwd= 's4UqmMqfhJVnNBlLn8s0KdUowOrYklkE',
     database= 'nasfat_render'
-    # host= 'localhost',
-    # user= 'mufatech',
-    # passwd = 'mufatech',
-    # database= 'nasfat2'
-    
 )
 
+
+# import mysql.connector
+
+# # Establish connection
+# connection = mysql.connector.connect(
+#     # host= 'localhost',
+#     # user= 'mufatech',
+#     # passwd = 'mufatech',
+#     # database= 'nasfat2'
+# )
+
+
 # Create cursor
-cursor = connection.cursor()
+# cursor = connection.cursor()
 
-# Execute SQL query
-cursor.execute("SHOW TABLES")
+# # Execute SQL query
+# cursor.execute("SHOW TABLES")
 
-# Fetch and display results
-tables = cursor.fetchall()
-for table in tables:
-    print(table[0])
+# # Fetch and display results
+# tables = cursor.fetchall()
+# for table in tables:
+#     print(table[0])
 
-# Close the cursor and connection
-cursor.close()
-connection.close()
+# # Close the cursor and connection
+# cursor.close()
+# connection.close()
