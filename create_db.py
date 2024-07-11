23e46fb5fe2b3bae32f48d572eadcc5e8270ae06
mufatech <fabosedesidiqat@gmail.com>
2024-07-11 13:54:02 +0100

    updated

--- create_db.py
+++ create_db.py
@@ -1,4 +1,21 @@
-# import mysql.connector
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host= 'dpg-cq7pkqdds78s73dbd020-a.oregon-postgres.render.com',
+    user= 'nasfat_render_user',
+    passwd = 's4UqmMqfhJVnNBlLn8s0KdUowOrYklkE',
+)
+
+my_cursor = mydb.cursor()
+
+my_cursor.execute("CREATE DATABASE nasfat_render")
+
+my_cursor.execute("SHOW DATABASES")
+for db in my_cursor:
+    print(db)
+    
+    
+#     import mysql.connector
 
 # mydb = mysql.connector.connect(
 #     host= 'localhost',
